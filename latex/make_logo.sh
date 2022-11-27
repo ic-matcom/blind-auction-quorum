761d167cc2a33547ca813887aba43fd81843350f
Alben <albenurquiza@gmail.com>
2022-11-27 11:46:12 -0500

    cap3, conclusions and recomendations

--- latex/make_logo.sh
+++ latex/make_logo.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-inkscape -D --export-filename=.\Graphics\uhlogo.pdf --export-latex .\Graphics\uhlogo.svg
+inkscape Graphics/uhlogo.svg --export-pdf=Graphics/uhlogo.pdf
+inkscape Graphics/gas_reveal.svg --export-pdf=Graphics/gas_reveal.pdf
+inkscape Graphics/gas_auctionEnd.svg --export-pdf=Graphics/gas_auctionEnd.pdf
